b95554d998012b761b11a324c8810ce5b9d81f9b
AlbertoArochal <arochaldev@gmail.com>
2023-03-25 19:04:48 +0100

    components and helpers added

--- src/components/PokemonOptions.vue
+++ src/components/PokemonOptions.vue
@@ -1,5 +1,12 @@
 <template>
-    <h3>Pokemon Options</h3>
+    <div class="options-container">
+        <ul>
+            <li>Option 1</li>
+            <li>Option 2</li>
+            <li>Option 3</li>
+            <li>Option 4</li>
+        </ul>
+    </div>
 </template>
 
 <script>
@@ -8,3 +15,27 @@ export default {
     components: {},
 };
 </script>
+
+<style scoped>
+/* Pokemon Options */
+ul {
+    list-style-type: none;
+}
+li {
+    background-color: white;
+    border-radius: 5px;
+    border: 1px solid rgba(0, 0, 0, 0.2);
+    cursor: pointer;
+    margin-bottom: 10px;
+    width: 250px;
+}
+
+li:hover {
+    background-color: rgba(0, 0, 0, 0.05);
+}
+
+.options-container {
+    display: flex;
+    justify-content: center;
+}
+</style>
--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,14 +1,15 @@
 <template>
     <h1>Who is this Pokemon?</h1>
+    <PokemonImage :pokemonId="4" :showPokemon="true" />
     <PokemonOptions />
-    <PokemonImage />
 </template>
-
+import PokemonOptions from "@/components/helpers/PokemonOptions.vue";
 <script>
 import PokemonOptions from "@/components/PokemonOptions.vue";
 import PokemonImage from "@/components/PokemonImage.vue";
 export default {
     name: "PokemonPage",
+
     components: {
         PokemonOptions,
         PokemonImage,
